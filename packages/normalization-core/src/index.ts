2a3e87f3bac09728f18f846762dda4c6d4e19032
Peter Steinberger <steipete@gmail.com>
2026-07-12 21:00:37 -0700

    refactor(normalization): consolidate agent ids

--- packages/normalization-core/src/index.ts
+++ packages/normalization-core/src/index.ts
@@ -1,5 +1,6 @@
 // Public barrel for shared coercion and normalization helpers.
 
+export * from "./agent-id.js";
 export * from "./boolean-coercion.js";
 export * from "./duration.js";
 export * from "./error-coercion.js";
